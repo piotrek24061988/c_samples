88a37040c509da8e84fa9d14359ad9cb7eb5006e
Piotr Gorecki <piotr.gorecki@globallogic.com>
2024-09-18 15:46:45 +0200

    encoding + decoding

--- main14.c
+++ main14.c
@@ -22,9 +22,11 @@ int isInputStringProvided(int argc, const char * argv[]) {
 //
 int isInputStringCorrect(const char * inputString, char * outputString) {
 	//Convert input letters to low letters
-	for(int i = 0; inputString[i]; i++) {
+	int i;
+	for(i = 0; inputString[i]; i++) {
 		outputString[i] = tolower(inputString[i]);
 	}
+	outputString[i] = '\0';
 	//Check if there are only letters in string
 	for(int i = 0; inputString[i]; i++) {
 		if( (outputString[i] < 'a') || (outputString[i] > 'z')) {
@@ -41,28 +43,70 @@ int isInputStringCorrect(const char * inputString, char * outputString) {
 // Perform encoding
 //
 void encode(const char * inputString, char * outputString) {
-	char prevChar = 0;
-	int counter = 0;
-	char count[10];
+
+	char prevChar = 0;//a
+	int counter = 0;//1
+	char buffer[10];
+	sprintf(buffer, "");
 	
-	for(int i = 0; inputString[i]; i++) {
-		if(inputString[i] == prevChar) {
-			counter++;
-		} else { 
-			if(prevChar != 0) {
-				sprintf(count, "%d", counter);
-				strcat(outputString, count);
-				sprintf(count, "%c", prevChar);
-				strncat(outputString, count, 1);
+	for(int i = 0; inputString[i]; i++)
+	{
+		if(inputString[i] != prevChar) {
+			//first sign
+			if(prevChar == 0) {
+				prevChar = inputString[i];
+				counter = 1;		
 			}
-			prevChar = inputString[i];
-			counter = 1;
+			//new sign
+			else {
+				sprintf(buffer, "%i", counter);
+				strcat(outputString, buffer);
+				sprintf(buffer, "%c", prevChar);
+				strcat(outputString, buffer);
+				
+				counter = 1;
+				prevChar = inputString[i];
+			}
+		}
+		//the same sign
+		else if(inputString[i] == prevChar) {
+			counter++;
 		}
 	}
-	sprintf(count, "%d", counter);
-	strcat(outputString, count);
-	sprintf(count, "%c", prevChar);
-	strncat(outputString, count, 1);
+	if((prevChar != 0) && (counter != 0)) {
+		sprintf(buffer, "%i", counter);
+		strcat(outputString, buffer);
+		sprintf(buffer, "%c", prevChar);
+		strcat(outputString, buffer);
+		strcat(outputString, "\0");
+	}
+}
+
+
+//
+// Perform decoding
+//
+void decode(const char * inputString, char * outputString) {
+	
+	int value = 0;
+	char currChar = 0;
+	
+	printf("decode: input string: %s\n", inputString);
+
+	for(int i = 0; inputString[i]; i = i+2) {
+		if(inputString[i+1] == '\0')
+			return;
+		//read number
+		sscanf(&inputString[i], "%*[^0123456789]%d", &value);
+		printf("  value[%i]:%i\n", i, value);
+		//read sign
+		sscanf(&inputString[i+1], "%*[^abcdefghijklmnopqrstuvwqyz]%c", &currChar);
+		printf("  currChar[%i]:%c\n", i, currChar);
+		
+		for(int j = 0; j < value; j++){
+			strncat(outputString, &currChar, 1);
+		}
+	}
 }
 
 int main(int argc, const char * argv[]) {
@@ -75,7 +119,8 @@ int main(int argc, const char * argv[]) {
 	//Create buffers
 	char inStr[strlen(argv[1])];
 	char outStr[strlen(argv[1])];
-	strcpy(outStr, "");
+	char outStr2[strlen(argv[1])];
+	sprintf(outStr2, "%c", '\0');
 	
 	//Preprocess input
 	if(isInputStringCorrect(argv[1], inStr) == -1) {
@@ -89,9 +134,14 @@ int main(int argc, const char * argv[]) {
 		printf("Lets encode: \n");
 	}
 	encode(inStr, outStr);
-	
-	//Print output
 	printf("Encoded input is: %s\n", outStr);
+	
+	//Decode back
+	if(outStr) {
+		printf("Lets decode back: \n");
+	}
+	decode(outStr, outStr2);
+	printf("Input decoded back is: %s\n", outStr2);
 
     return 0;
 }
