869fec24d6d14c2710a48302eefd42da2123b590
Piotr Gorecki <piotr.gorecki@globallogic.com>
2024-09-23 16:19:24 +0200

    buble sort improved

--- main16.c
+++ main16.c
@@ -6,17 +6,43 @@
 
 
 //Complexity is O(n^2)
+//[0] [1] [2]
+// 3   2   1
+// 3   1   2
+// 1   3   2
+// 1   2   3
+
+//0:
+//[0] [1] [2]
+// 3   2   1
+
+//1: i = 2, j = 2
+//[0] [1] [2]
+// 3   1   2
+
+// i = 2, j = 1:
+//[0] [1] [2]
+// 1   3   2
+
+// i = 1, j = 2
+//[0] [1] [2]
+// 1   2   3
+
+// i = 1, j = 1
+//[0] [1] [2]
+// 1   2   3
+
 void babSort(const char * in, char * out) {
 	
 	strcpy(out, in);
 	
-	for(int i = 0; i < strlen(out); i++){
-		for(int j = 0; j < strlen(out) - i - 1; j++)
-			if(out[j] > out[j + 1]) {
-				SWAP(out[j], out[j + 1]);
-			}
+	for(int i = strlen(out) - 1; i > 0; i--) {
+		for(int j = strlen(out) - 1; j >= strlen(out) - i; j--)
+			if(out[j] < out[j-1])
+				SWAP(out[j-1], out[j])
 	}
 }
+
 
 int main(int argc, const char * argv[]) {
 	
@@ -30,7 +56,6 @@ int main(int argc, const char * argv[]) {
 	
 	printf("Input string is: %s\n", toSort);
 	babSort(toSort, sorted);
-
 	printf("Output string is: %s\n", sorted);
 
     return 0;
